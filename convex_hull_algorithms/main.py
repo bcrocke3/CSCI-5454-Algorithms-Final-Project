d7d158750f60448e2bdc0027653160f574096fe1
Charles Wade <colson72098@gmail.com>
2023-04-18 10:09:35 -0600

    Adds functions to generate and draw 3D points

--- convex_hull_algorithms/main.py
+++ convex_hull_algorithms/main.py
@@ -12,8 +12,12 @@ NDFloatArray = npt.NDArray[np.float64]
 NDIntArray = npt.NDArray[np.int64]
 
 
-def gen_n_random_points(n: int, min: float, max: float) -> NDFloatArray:
+def gen_n_random_points_2d(n: int, min: float, max: float) -> NDFloatArray:
     return np.random.uniform(min, max, (n, 2))
+
+
+def gen_n_random_points_3d(n: int, min: float, max: float) -> NDFloatArray:
+    return np.random.uniform(min, max, (n, 3))
 
 
 if __name__ == '__main__':
@@ -25,7 +29,7 @@ if __name__ == '__main__':
                                          [0.5, 0.5]],
                                         dtype=np.float64)
 
-    # test_input: NDFloatArray = gen_n_random_points(1000, 0, 100)
+    # test_input: NDFloatArray = gen_n_random_points_2d(1000, 0, 100)
 
     test_answer: NDIntArray = np.array([[1], [1], [1], [1], [0]], dtype=np.int64)
 
@@ -37,3 +41,21 @@ if __name__ == '__main__':
     visualize.draw2D(test_input, dc_result, "Divide and Conquer Result")
     visualize.draw2D(test_input, quickhull_result, "Quickhull Result")
     visualize.draw2D(test_input, test_answer, "Hard-Coded Answer")
+
+    test_input_3d: NDFloatArray = np.array([[0.0, 1.0, 0.0],
+                                            [1.0, 1.0, 0.0],
+                                            [1.0, 0.0, 0.0],
+                                            [0.0, 0.0, 0.0],
+                                            [0.5, 0.5, 0.0],
+                                            [0.0, 1.0, 1.0],
+                                            [1.0, 1.0, 1.0],
+                                            [1.0, 0.0, 1.0],
+                                            [0.0, 0.0, 1.0],
+                                            [0.5, 0.5, 1.0]],
+                                             dtype=np.float64)
+
+    # test_input_3d: NDFloatArray = gen_n_random_points_3d(10, 0, 100)
+
+    test_answer_3d: NDIntArray = np.array([[1], [1], [1], [1], [0], [1], [1], [1], [1], [0]], dtype=np.int64)
+
+    visualize.draw3D(test_input_3d, test_answer_3d, "3D Result")
--- convex_hull_algorithms/visualize.py
+++ convex_hull_algorithms/visualize.py
@@ -30,3 +30,29 @@ def draw2D(points: NDFloatArray, colors: NDIntArray, title: str = "Convex Hull R
     plt.scatter(points[:, 0], points[:, 1], c=colormap[colors[:, 0]])
     plt.title(title)
     plt.show()
+
+
+def draw3D(points: NDFloatArray, colors: NDIntArray, title: str = "Convex Hull Results") -> None:
+    """ Plots 3D points in multiple colors
+
+    :param points: 2D array of points to plot. Each row is a point. Must have 3 columns for x, y, z dimensions
+    :param colors: Column vector of category/color that corresponds to each point
+    :param title: title of output graph. Should describe how input data was computed
+    :return: Nothing. Draws plots.
+    """
+    # check assumptions
+    assert(len(points.shape) == 2)  # 2D points matrix
+    assert(points.shape[1] == 3)    # all the points are 3D (3, y)
+    assert(len(colors.shape) == 2)  # colors is column vector
+    assert(colors.shape[1] == 1)
+    assert(points.shape[0] == colors.shape[0])  # points and colors have same # of rows
+
+    # define colors
+    colormap = np.array(['black', 'red'])
+
+    # graph
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=colormap[colors[:, 0]])
+    plt.title(title)
+    plt.show()
